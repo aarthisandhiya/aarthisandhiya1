84283a38f500edf0b123a9fb892d544d80c878f1
aarthisandhiya <46442329+aarthisandhiya@users.noreply.github.com>
2019-02-19 23:22:54 +0530

    Update p11.py

--- p11.py
+++ p11.py
@@ -1,14 +1,3 @@
-a,b=map(int,input().split())
-x=' '
-for num in range(a+1,b):
-   if num > 1:
-       for i in range(2,num):
-           if (num % i) == 0:
-               break
-       else:
-           print(num,end=" ")
-
-player11.py
 a=input()
 if a=='Sunday' or a=='Saturday':
 	print("yes")
