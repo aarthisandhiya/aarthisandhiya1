c88094aa9c386961451819b8248aaedf660ba089
aarthisandhiya <46442329+aarthisandhiya@users.noreply.github.com>
2019-03-05 09:41:35 +0530

    Create 106.py

---
+++ 106.py
@@ -0,0 +1,9 @@
+a,b=map(int,input().split())
+c = [int(a) for a in input().split()]
+def sq(self):
+    if c[b]%2!=0:
+        print(c[b])
+    else:
+        c[b]=c[b-1]
+        print(c[b])
+sq(c)
