966e2b92aa5f714b475d672becd10111a809709a
aarthisandhiya <46442329+aarthisandhiya@users.noreply.github.com>
2019-03-15 12:09:35 +0530

    Create 52player.py

---
+++ 52player.py
@@ -0,0 +1,15 @@
+ a,b=map(int,input().split())
+s=[int(a) for a in input().split()]
+s=list(s)
+z=[]
+for i in range(0,len(s)):
+	val=s[i]
+	i=i-1
+	while i>=0:
+		if val<s[i]:
+			s[i+1]=s[i]
+			s[i]=val
+			i=i-1
+		else:
+			break
+print(s[b-1])
