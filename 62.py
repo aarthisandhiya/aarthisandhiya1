fa479473f867779f411ccf08d304255736bb77f2
aarthisandhiya <46442329+aarthisandhiya@users.noreply.github.com>
2019-03-16 09:40:40 +0530

    Update 62.py

--- 62.py
+++ 62.py
@@ -2,8 +2,11 @@ a=int(input())
 c=0
 e=[]
 for i in range(1,a):
-	if a%i==0 and (a//i)%2==1:
-		if i>c:
-			e.append(i)
+	if a%i==0 :
+	    if (a//i)%2==1:
+		    if i>c:
+			    e.append(i)
+	    else:
+	       e.append(a)
 print(e[0])
 	
