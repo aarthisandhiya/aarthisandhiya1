32f9c3109cf3ee5d623323105209feda4ea56ffa
aarthisandhiya <46442329+aarthisandhiya@users.noreply.github.com>
2019-03-16 11:23:39 +0530

    Update 63.py

--- 63.py
+++ 63.py
@@ -2,5 +2,10 @@ a=int(input())
 b=[int(a) for a in input().split()]
 c=[int(a) for a in input().split()]
 d=[x for x in b if x in c]
-string=' '.join(str(e) for e in d)
+#d=list(dict.fromkeys(d))or the below can also used
+s = []
+for i in d:
+       if i not in s:
+          s.append(i)
+string=' '.join(str(e) for e in s)
 print(string)
