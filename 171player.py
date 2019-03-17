422e325f36a4ebaa0385b322c7b8dbcdcc7c829d
aarthisandhiya <46442329+aarthisandhiya@users.noreply.github.com>
2019-03-17 17:36:11 +0530

    Update 171player.py

--- 171player.py
+++ 171player.py
@@ -3,9 +3,12 @@ c=[]
 for i in a.split():
     c.append(i)
 temp=''
-for i in range(0,len(c)):
-    if c[i]=='and':
-        temp=c[i-i]
-        c[i-1]=c[i+1]
-        c[i+1]=temp
-print(' '.join(c))
+if len(c)==1 and c[0]=='and':
+    print("and")
+else:
+    for i in range(0,len(c)):
+        if c[i]=='and':
+            temp=c[i-i]
+            c[i-1]=c[i+1]
+            c[i+1]=temp
+    print(' '.join(c))
