2600c8887255583d011144d4a1fdeeae7f4c3ebb
aarthisandhiya <46442329+aarthisandhiya@users.noreply.github.com>
2019-05-13 12:26:34 +0530

    Update player 101.py

--- player 101.py
+++ player 101.py
@@ -1,6 +1,10 @@
 a=int(input())
 s=0
 b=[int(a) for a in input().split()]
-for i in range(1,len(b)):
-    s=s+b[i]
+for i in range(0,len(b)-1):
+    j=i+1
+    if b[i]>b[j]:
+        s=s+b[i]
+    else:
+        s=s+b[j]
 print(s)
