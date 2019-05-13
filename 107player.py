a81b94699945311f176d3c5fee4ff88254c4014d
aarthisandhiya <46442329+aarthisandhiya@users.noreply.github.com>
2019-05-13 14:37:08 +0530

    Update 107player.py

--- 107player.py
+++ 107player.py
@@ -1,6 +1,6 @@
-str1=input().split()
-str2=input().split()
-for i in range(0,len(str1)):
-    for j in range(0,len(str2)):
-        if str2[j]==str1[i]:
+str11=input().split()
+str22=input().split()
+for i in range(0,len(str11)):
+    for j in range(0,len(str22)):
+        if str22[j]==str11[i]:
             print(i+1)
