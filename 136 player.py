9e51549490f026f78f10a7600abae54ce173d926
aarthisandhiya <46442329+aarthisandhiya@users.noreply.github.com>
2019-05-14 10:15:20 +0530

    Create 136 player.py

---
+++ 136 player.py
@@ -0,0 +1,10 @@
+a,b=map(int,input().split())
+count=0
+c=[int(a) for a in input().split()]
+for i in range(0,len(c)):
+    if c[i]==b:
+        count=count+1 
+if count==0:
+    print("no")
+else:
+    print("yes",' ',count)
