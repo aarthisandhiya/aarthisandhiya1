3fb22f0cb5e73749219352e001cf8c035bcf9552
aarthisandhiya <46442329+aarthisandhiya@users.noreply.github.com>
2019-05-14 12:08:10 +0530

    Update 141player.py

--- 141player.py
+++ 141player.py
@@ -4,11 +4,11 @@ c=0
 for i in range(0,a):
     e=input()
     s.append(e)
-for i in s:
-    for j in s:
-        if i==j:
-            c=c+1 
-if c>1:
+for i in range(0,len(s)-1):
+    j=i+1 
+    if str(s[i])==str(s[j]):
+        c=c+1 
+if c>=1:
     print("yes")
 else:
     print("no")
