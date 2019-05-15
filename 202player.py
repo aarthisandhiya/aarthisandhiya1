c8c6adc9b534c0df0987632aeafce6cfa9022c96
aarthisandhiya <46442329+aarthisandhiya@users.noreply.github.com>
2019-05-15 13:43:56 +0530

    Create 202player.py

---
+++ 202player.py
@@ -0,0 +1,15 @@
+a=input()
+b=[]
+b.append(a)
+s=[]
+d=[]
+for i in range(0,len(b)):
+    if b[i]=='a' or b[i]=='e'  or b[i]=='o' or b[i]=='u' or b[i]=='A' or b[i]=='E' or b[i]=='O' or b[i]=='U' or b[i]=='i' or b[i]=='I':    
+        s.append(b[i])
+    else:
+        d.append(b[i])
+c=s+ d 
+for i in range(0,len(c)):
+    print(c[i],end="")
+        
+    
