70706e1a4a34d96eadcaad596c9028523d6f0f75
aarthisandhiya <46442329+aarthisandhiya@users.noreply.github.com>
2019-05-18 12:25:42 +0530

    Update 186player.py

--- 186player.py
+++ 186player.py
@@ -8,7 +8,7 @@ for i in a:
         c=c+1
     elif i=="Sundar":
         d=d+1
-    elif i=="VishalSundar":
+    elif i=="VishalSundar" or i=="SundarVishal":
         e=e+1
 if c>0 and d>0 or e>0:
     print("yes")
