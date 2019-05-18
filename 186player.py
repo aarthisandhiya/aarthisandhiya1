721971af9363856aa102175bd0e888094930e290
aarthisandhiya <46442329+aarthisandhiya@users.noreply.github.com>
2019-05-18 12:25:08 +0530

    Update 186player.py

--- 186player.py
+++ 186player.py
@@ -2,12 +2,15 @@ a=input().split()
 a=list(a)
 c=0
 d=0
+e=0
 for i in a:
     if i=="Vishal":
         c=c+1
     elif i=="Sundar":
         d=d+1
-if c>0 and d>0:
+    elif i=="VishalSundar":
+        e=e+1
+if c>0 and d>0 or e>0:
     print("yes")
 else:
     print("no")
