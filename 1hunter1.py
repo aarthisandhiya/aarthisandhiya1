207edad3e1fa59b9d9cfd971b1154ed677847448
aarthisandhiya <46442329+aarthisandhiya@users.noreply.github.com>
2019-05-20 10:51:24 +0530

    Rename 1hunter.py to 1hunter1.py

---
+++ 1hunter1.py
@@ -0,0 +1,11 @@
+n=int(input())
+lis=list(map(int,input().split()))
+t=[]
+for i in lis:
+    if lis.count(i)>1:
+        t.append(i)
+x=set(t)
+if len(x)==0:
+    print("unique")
+else:
+    print(*x)
